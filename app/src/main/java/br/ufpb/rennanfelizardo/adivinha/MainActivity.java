9a4de59aaba5d43982e49138e26fa8354a3493b3
Rennan Felizardo <rennan.felizardo@dce.ufpb.br>
2016-08-25 07:57:21 -0300

    Projeto pronto

---
+++ app/src/main/java/br/ufpb/rennanfelizardo/adivinha/MainActivity.java
@@ -0,0 +1,34 @@
+package br.ufpb.rennanfelizardo.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaoJogarId);
+        textoResultado = (TextView) findViewById(R.id.resutadoId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int numeroAleatorio = random.nextInt(11);
+
+                textoResultado.setText("Número escolhido: " + numeroAleatorio);
+            }
+        });
+    }
+}
